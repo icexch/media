0d97be53dd29c1edd01f2058153af67de4e960e7
alex <wish@mail.ru>
2018-02-15 14:57:22 +0300

    fix users seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,7 +13,7 @@ class UserTableSeeder extends Seeder
     {
         $faker = Faker\Factory::create();
 
-        for($i=0; $i<3; $i++) {
+        for ($i = 0; $i < 3; $i++) {
             $this->createUserInstance(\App\Models\User\Moderator::class, $faker);
             $this->createUserInstance(\App\Models\User\Publisher::class, $faker);
             $this->createUserInstance(\App\Models\User\Advertiser::class, $faker);
@@ -28,11 +28,19 @@ class UserTableSeeder extends Seeder
      */
     protected function createUserInstance($userClass, $faker)
     {
-        return $userClass::create([
-            'name' => $faker->name,
-            'email' => $faker->email,
+        $user = $userClass::create([
+            'name'     => $faker->name,
+            'email'    => $faker->email,
             'password' => 'test',
-            'balance' => $faker->randomFloat(9, 0, 1000)
+            'balance'  => $faker->randomFloat(9, 0, 1000)
+        ]);
+
+        $profile = \App\Models\UserProfile::create([
+            'user_id'      => $user->id,
+            'company_name' => $faker->company,
+            'phone'        => 123456789,
+            'city'         => $faker->city,
+            'country'      => $faker->country
         ]);
     }
 }
