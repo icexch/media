f40c39acc3c4e8e938d5e96a06e9807826922299
alex <wish@mail.ru>
2018-03-03 23:25:25 +0300

    add role redirects

--- app/Http/Middleware/Role.php
+++ app/Http/Middleware/Role.php
@@ -18,11 +18,23 @@ class Role
         $user = $request->user();
 
         if (
-               ($user->isModerator() && in_array(User::MODERATOR_SLUG, $roles))
+            ($user->isModerator() && in_array(User::MODERATOR_SLUG, $roles))
             || ($user->isAdvertiser() && in_array(User::ADVERTISER_SLUG, $roles))
             || ($user->isPublisher() && in_array(User::PUBLISHER_SLUG, $roles))
         ) {
             return $next($request);
+        }
+
+        if ($user->isModerator()) {
+            return redirect()->route('admin.dashboard');
+        }
+
+        if ($user->isAdvertiser()) {
+            return redirect()->route('advertiser.dashboard');
+        }
+
+        if ($user->isPublisher()) {
+            return redirect()->route('publisher.dashboard');
         }
 
         return abort(403);
