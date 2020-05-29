c75a22322f566cd6489c1c7dbb517bd2438b4971
Andrew Skurlatov <a.skurlatov@gmail.com>
2020-05-30 01:02:16 +0300

    fix: storafe permissions

--- start.sh
+++ start.sh
@@ -12,4 +12,7 @@ docker-compose run --rm cli php artisan key:generate --no-interaction
 
 docker-compose run --rm cli php artisan migrate --seed --no-interaction
 
+chmod -R 777 /var/www/laravel/storage
+chmod -R 777 /var/www/laravel/bootstrap/cache
+
 docker-compose up -d redis db app nginx
